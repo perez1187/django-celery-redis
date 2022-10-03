e173140d3b0d77b5e9ab748891c0d03f2faa0f5b
perez1187 <o.perez1187@gmail.com>
2022-10-03 15:02:02 +0200

    test

---
+++ fib/managment/commands/delete_calculations.py
@@ -0,0 +1,8 @@
+
+
+from django.core.management.base import BaseCommand
+from fib.models import Calculation
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Calculation.objects.all().delete()
